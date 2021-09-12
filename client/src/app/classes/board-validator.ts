4445dd8f328e45dd39231a2e033366a4f4e801f1
Julien Bourque <julien.bourque@polymtl.ca>
2021-09-12 12:12:44 -0400

    Added board service

--- client/src/app/classes/board-validator.ts
+++ client/src/app/classes/board-validator.ts
@@ -1,11 +1,119 @@
-import { Board } from './board';
+import { BoardError } from '@app/exceptions/board-error';
+import { Board, ImmutableBoard } from './board';
+import { Square } from './square';
 import { ValidationResponse } from './validation-response';
 import { Vec2 } from './vec2';
 
-export class BoardValidator {
-    constructor(private board: Board) {}
+enum Direction {
+    Horizontal,
+    Vertical,
+    None,
+}
 
-    validateWord(letters: [string, Vec2][]): ValidationResponse {
-        return { isSuccess: true, description: '', points: 1 };
+export class BoardValidator {
+    constructor(private board: ImmutableBoard, private lookup: (word: string) => boolean, letterValues: { [key: string]: number }) { }
+
+    private static validateIfSquareFilled(square: Square | null): boolean {
+        return square != null && square.letter !== '';
+    }
+
+    private static sortLetters(letters: [string, Vec2][], direction: Direction): [string, Vec2][] {
+        if (direction === Direction.Horizontal) {
+            return letters.sort((l1, l2) => l1[1].x - l2[1].x);
+        } else {
+            return letters.sort((l1, l2) => l1[1].y - l2[1].y);
+        }
+    }
+
+    validate(letters: [string, Vec2][]): ValidationResponse {
+        if (letters.length === 0) {
+            return { isSuccess: false, description: 'Empty placement', points: 0 };
+        }
+
+        const positions: Vec2[] = letters.map((e) => e[1]);
+        const clonedBoard = this.board.clone();
+
+        try {
+            clonedBoard.merge(letters);
+        } catch (error) {
+            if (error instanceof BoardError) {
+                return { isSuccess: false, description: error.message, points: 0 };
+            }
+
+            throw error;
+        }
+
+        if (this.board.filledSquare === 0) {
+            if (!this.validateFirstPlacement(letters)) return { isSuccess: false, description: 'Invalid first word', points: 0 };
+        } else if (!this.ensureCoherence(positions)) {
+            return { isSuccess: false, description: 'Invalid coherence', points: 0 };
+        }
+
+        const direction: Direction = this.retrieveDirection(positions);
+
+        if (direction === Direction.None) {
+            return { isSuccess: false, description: 'Letters needs to be placed on the same line', points: 0 };
+        }
+
+        const sortedLetters = BoardValidator.sortLetters(letters, direction);
+
+        return this.validateWords(clonedBoard, sortedLetters, direction);
+    }
+
+    private validateFirstPlacement(letters: [string, Vec2][]): boolean {
+        if (letters.length < 2) {
+            return false;
+        }
+        for (const [, position] of letters) {
+            if (position.x === 0 && position.y === 0) return true;
+        }
+        return false;
+    }
+
+    private ensureCoherence(positions: Vec2[]): boolean {
+        for (const position of positions) {
+            if (
+                BoardValidator.validateIfSquareFilled(this.board.left(position)) ||
+                BoardValidator.validateIfSquareFilled(this.board.right(position)) ||
+                BoardValidator.validateIfSquareFilled(this.board.up(position)) ||
+                BoardValidator.validateIfSquareFilled(this.board.down(position))
+            ) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private retrieveDirection(positions: Vec2[]): Direction {
+        if (positions.length < 2) return Direction.None;
+
+        const firstPosition: Vec2 = positions[0];
+        const secondPosition: Vec2 = positions[1];
+
+        let direction: Direction;
+
+        if (firstPosition.x === secondPosition.x) {
+            direction = Direction.Vertical;
+        } else if (firstPosition.y === secondPosition.y) {
+            direction = Direction.Horizontal;
+        } else {
+            return Direction.None;
+        }
+
+        for (let i = 2; i < positions.length; i++) {
+            if (positions[i - 1].x === positions[i].x) {
+                if (direction !== Direction.Vertical) return Direction.None;
+            } else if (positions[i - 1].y === positions[i].y) {
+                if (direction !== Direction.Horizontal) return Direction.None;
+            } else {
+                return Direction.None;
+            }
+        }
+
+        return direction;
+    }
+
+    private validateWords(clonedBoard: Board, sortedLetters: [string, Vec2][], direction: Direction): ValidationResponse {
+        // TODO
     }
 }
--- client/src/app/services/board.service.ts
+++ client/src/app/services/board.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Board, IBoard } from '@app/classes/board';
+import { Board, ImmutableBoard } from '@app/classes/board';
 import { BoardValidator } from '@app/classes/board-validator';
 import { Bonus } from '@app/classes/bonus';
 import { ValidationResponse } from '@app/classes/validation-response';
 import { Vec2 } from '@app/classes/vec2';
 import { Constants } from '@app/constants/global.constants';
 import JsonBonuses from '@assets/bonus.json';
+import JsonLetters from '@assets/letters.json';
 
 @Injectable({
     providedIn: 'root',
@@ -16,19 +17,19 @@ export class BoardService {
 
     constructor() {
         this.board = new Board(Constants.grid.gridSize, this.retrieveBonuses());
-        this.boardValidator = new BoardValidator(this.board);
+        this.boardValidator = new BoardValidator(this.board, this.lookup, this.retrieveLetterValues());
     }
 
-    get gameBoard(): IBoard {
+    get gameBoard(): ImmutableBoard {
         return this.board;
     }
 
     validateWord(letters: [string, Vec2][]): ValidationResponse {
-        return this.boardValidator.validateWord(letters);
+        return this.boardValidator.validate(letters);
     }
 
     addWord(letters: [string, Vec2][]): ValidationResponse {
-        const response = this.boardValidator.validateWord(letters);
+        const response = this.boardValidator.validate(letters);
 
         if (!response.isSuccess) return response;
 
@@ -37,13 +38,28 @@ export class BoardService {
         return response;
     }
 
-    private retrieveBonuses(): { [key: string]: Bonus } {
-        const bonuses: { [key: string]: Bonus } = {};
+    private retrieveBonuses(): [Vec2, Bonus][] {
+        const bonuses: [Vec2, Bonus][] = new Array<[Vec2, Bonus]>();
 
         for (const jsonBonus of JsonBonuses) {
-            bonuses[jsonBonus.x + jsonBonus.y] = Bonus[jsonBonus.bonus as keyof typeof Bonus];
+            bonuses.push([jsonBonus.Position, Bonus[jsonBonus.Bonus as keyof typeof Bonus]]);
         }
 
         return bonuses;
+    }
+
+    private retrieveLetterValues(): { [key: string]: number } {
+        const letterValues: { [key: string]: number } = {};
+
+        for (const jsonLetter of JsonLetters) {
+            letterValues[jsonLetter.Letter] = jsonLetter.Value;
+        }
+
+        return letterValues;
+    }
+
+    // TODO Implement dictionary service
+    private lookup(word: string): boolean {
+        return word === 'test';
     }
 }
