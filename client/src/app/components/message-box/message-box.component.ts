603f54617adb2c3435a67a3d32833ad851e57fe5
ehourdebaigt <etienne.hourdebaigt@polymtl.ca>
2021-09-15 16:15:24 -0400

    EH: Add Key press action and autofocus on Input Field

--- client/src/app/components/message-box/message-box.component.ts
+++ client/src/app/components/message-box/message-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { Message } from '@app/classes/message';
 import { CommandsService } from '@app/services/commands/commands.service';
 import { MessagesService } from '@app/services/messages/messages.service';
@@ -6,12 +6,13 @@ import { MessagesService } from '@app/services/messages/messages.service';
     selector: 'app-message-box',
     templateUrl: './message-box.component.html',
     styleUrls: ['./message-box.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MessageBoxComponent implements OnInit {
-    @Input() messages: Message[]; // buffer
-    value: string; // Text input value
+    @Input() messages: Message[];
+    value: string; // Command input value so that we can use the "x" to erase the content
 
-    constructor(private commandsService: CommandsService, private messagesService: MessagesService) {}
+    constructor(private commandsService: CommandsService, public messagesService: MessagesService) {}
 
     ngOnInit(): void {
         this.messagesService.getMessages().subscribe((messages) => (this.messages = messages));
