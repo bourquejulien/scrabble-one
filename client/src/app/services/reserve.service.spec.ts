f3d769f0c759b69326ddaf023dfdc84c38ca8a19
Eloise Brosseau <eloise.brosseau@polymtl.ca>
2021-09-17 14:25:22 -0400

    Add reserve service test

--- client/src/app/services/reserve.service.spec.ts
+++ client/src/app/services/reserve.service.spec.ts
@@ -1,16 +1,54 @@
 import { TestBed } from '@angular/core/testing';
-
 import { ReserveService } from './reserve.service';
+
 
 describe('ReserveService', () => {
     let service: ReserveService;
+    let letterToExchange: string;
+    let mockReserve: string[];
 
     beforeEach(() => {
         TestBed.configureTestingModule({});
         service = TestBed.inject(ReserveService);
+        letterToExchange = 'A';
+        mockReserve = ['A', 'A', 'A', 'B', 'B', 'C', 'C'];
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
     });
+
+    it('should increase length of reserve', () => {
+        const currentLength = service.length;
+
+        service.putBackLetter(letterToExchange);
+        expect(service.length).toBe(currentLength + 1);
+    });
+
+    it('should successfully remove a letter from reserve', () => {
+        service.putBackLetter(letterToExchange);
+        expect(service.drawLetter).toBe('B');
+    });
+
+    it('should decrease length in reserve', () => {
+        const currentLength = service.length;
+
+        service.drawLetter();
+        expect(service['reserve'].length).toBe(currentLength - 1);
+    });
+
+    it('should successfully return a letter from reserve', () => {
+        spyOn(Math, 'floor').and.returnValue(10);
+        expect(service.drawLetter).toBe('B');
+    });
+
+    it('should successfully return a letter from reserve', () => {
+        spyOn(Math, 'floor').and.returnValue(10);
+        expect(service.drawLetter).toBe('B');
+    });
+
+    it('should return reserve length', () => {
+        expect(service.length).toBe(service['reserve'].length);
+    });
 });
+
--- client/src/app/services/reserve.service.ts
+++ client/src/app/services/reserve.service.ts
@@ -20,14 +20,15 @@ export class ReserveService {
         this.reserve.splice(letterIndex, 0, letterToExchange);
     }
 
+    // test edge case for out of bounds index???
     drawLetter(): string {
-        const randomLetterIndex = Math.floor(Math.random() * (this.reserve.length));
+        const randomLetterIndex = Math.floor(Math.random() * this.reserve.length);
         this.reserve.splice(randomLetterIndex, 1);
 
         return this.reserve[randomLetterIndex];
     }
 
-    getLength(): number {
+    get length(): number {
         return this.reserve.length;
     }
 }
