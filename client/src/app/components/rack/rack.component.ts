cb432a474e5cd25d57579cbeaea9035ea755aad7
Eloise Brosseau <eloise.brosseau@polymtl.ca>
2021-09-20 19:36:11 -0400

    Create rack with reserve service

--- client/src/app/components/rack/rack.component.ts
+++ client/src/app/components/rack/rack.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { letterDefinitions } from '@app/classes/letter';
+import { ReserveService } from '@app/services/reserve.service';
 
 @Component({
     selector: 'app-rack',
     templateUrl: './rack.component.html',
     styleUrls: ['./rack.component.scss'],
 })
-export class RackComponent {
-    rack = ['A', 'B', 'C'];
+export class RackComponent implements OnInit {
+    rack: string[] = [];
+
+    constructor(private reserveService: ReserveService) {}
+
+    ngOnInit() {
+        const initNbTiles = 7;
+
+        for (let tile = 0; tile < initNbTiles; tile++) {
+            this.rack.push(this.reserveService.drawLetter());
+        }
+    }
 
     retrievePoints(letter: string): number {
         const currentLetterData = letterDefinitions.get(letter);
