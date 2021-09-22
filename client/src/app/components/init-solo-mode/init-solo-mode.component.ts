2fac7372dee22ce8dcef13739d1b14768fb67f53
Morgan De Gregorio Beaudoin <morgan.de-gregorio-beaudoin@polymtl.ca>
2021-09-22 10:59:31 -0400

    Added gameConfig as interface

--- client/src/app/components/init-solo-mode/init-solo-mode.component.ts
+++ client/src/app/components/init-solo-mode/init-solo-mode.component.ts
@@ -14,22 +14,36 @@ export class InitSoloModeComponent implements OnInit {
     readonly botNames = Constants.botNames;
     readonly minutesList = Constants.turnLengthMinutes;
     readonly secondsList = Constants.turnLengthSeconds;
-    firstPlayerName: string;
-    secondPlayerName: string;
     nameForm: FormGroup;
 
     constructor(public game: GameService) {}
 
     ngOnInit(): void {
-        this.secondPlayerName = this.randomizeBotName(this.botNames);
+        this.game.gameConfig.secondPlayerName = this.randomizeBotName(this.botNames);
     }
 
     botNameChange(firstPlayerName: string): void {
-        while (firstPlayerName === this.secondPlayerName) {
-            this.secondPlayerName = this.randomizeBotName(Constants.botNames);
+        while (firstPlayerName === this.game.gameConfig.secondPlayerName) {
+            this.game.gameConfig.secondPlayerName = this.randomizeBotName(Constants.botNames);
         }
     }
-    confirmInitialization(name: string): boolean {
+
+    initialize(name: string): void {
+        const needsToReroute: boolean = this.confirmInitialization(name);
+        if (needsToReroute) {
+            // reroute
+        }
+    }
+
+    randomizeBotName(nameArr: string[]): string {
+        const randomIndex = Math.floor(Math.random() * nameArr.length);
+        return nameArr[randomIndex];
+    }
+
+    isNameValidator(): ValidatorFn {
+        return this.nameValidatorFunction as ValidatorFn;
+    }
+    private confirmInitialization(name: string): boolean {
         const nameForm = new FormGroup({
             control: new FormControl(name, [
                 Validators.required,
@@ -41,30 +55,20 @@ export class InitSoloModeComponent implements OnInit {
         this.nameForm = nameForm;
         if (nameForm.valid) {
             this.game.randomizeTurn();
-            this.firstPlayerName = name;
-            this.botNameChange(this.firstPlayerName);
+            this.game.gameConfig.firstPlayerName = name;
+            this.botNameChange(this.game.gameConfig.firstPlayerName);
             // Had to cast the parts of the addition to Numbers otherwise it was considered as a string
-            this.game.time = Number(this.game.minutes * Constants.timeConstant) + Number(this.game.seconds);
+            this.game.gameConfig.time = Number(this.game.gameConfig.minutes * Constants.timeConstant) + Number(this.game.gameConfig.seconds);
             return true;
         }
         return false;
     }
-
-    randomizeBotName(nameArr: string[]): string {
-        const randomIndex = Math.floor(Math.random() * nameArr.length);
-        return nameArr[randomIndex];
-    }
-
-    isNameValidator(): ValidatorFn {
-        return this.nameValidatorFunction as ValidatorFn;
-    }
-
     private nameValidatorFunction(control: FormControl): { [key: string]: boolean } | null {
         // We make sure that player name is considered as a string
         const playerName = control.value as string;
         if (playerName !== undefined && playerName !== null) {
             for (let index = 0; index < playerName.length; index++) {
-                if (!/[a-zA-Z]/.test(playerName.charAt(index))) return { ['containsOnlyLetters']: true };
+                if (!/[a-zA-Z||ÉéÎîÉéÇçÏï]/.test(playerName.charAt(index))) return { ['containsOnlyLetters']: true };
             }
             const firstLetter = playerName[0];
             if (firstLetter !== firstLetter.toUpperCase()) {
--- client/src/app/services/game.service.ts
+++ client/src/app/services/game.service.ts
@@ -1,17 +1,26 @@
 import { Injectable } from '@angular/core';
+import { GameConfig } from '@app/classes/game-config';
 import { Constants } from '@app/constants/global.constants';
 import { BehaviorSubject } from 'rxjs';
-
 @Injectable({
     providedIn: 'root',
 })
 export class GameService {
-    time: number;
-    gameType: string = Constants.gameTypesList[0];
-    minutes: number = Constants.turnLengthMinutes[1];
-    seconds: number = Constants.turnLengthSeconds[0];
     onTurn = new BehaviorSubject<boolean>(false);
-
+    time: number;
+    gameType: string;
+    minutes: number;
+    seconds: number;
+    firstPlayerName: string;
+    secondPlayerName: string;
+    gameConfig: GameConfig = {
+        gameType: Constants.gameTypesList[0],
+        minutes: Constants.turnLengthMinutes[1],
+        seconds: Constants.turnLengthSeconds[0],
+        time: 0,
+        firstPlayerName: '',
+        secondPlayerName: '',
+    };
     randomizeTurn(): boolean {
         const turn = Math.random() < Constants.half;
         this.onTurn.next(turn);
