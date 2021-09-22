0794ba8d59c923931151b4b69859e50d4b73de41
Julien Bourque <julien.bourque@polymtl.ca>
2021-09-22 12:03:26 -0400

    Added empty stubds to virtual player spec

--- client/src/app/services/virtual-player.service.spec.ts
+++ client/src/app/services/virtual-player.service.spec.ts
@@ -1,11 +1,30 @@
+/* eslint-disable max-classes-per-file -- Multiple stubs are used */
+import { Injectable } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { VirtualPlayerService } from '@app/services/virtual-player.service';
+import { BoardService } from './board.service';
+import { DictionaryService } from './dictionary.service';
+
+@Injectable({
+    providedIn: 'root',
+})
+class StubDictionaryService {}
+
+@Injectable({
+    providedIn: 'root',
+})
+class BoardServiceStub {}
 
 describe('VirtualPlayerService', () => {
     let service: VirtualPlayerService;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            providers: [
+                { provide: BoardService, useClass: BoardServiceStub },
+                { provide: DictionaryService, useClass: StubDictionaryService },
+            ],
+        });
         service = TestBed.inject(VirtualPlayerService);
     });
 
