bec44ce294ac9c9a767e8b9ed781d405cf8122e2
Julien Bourque <julien.bourque@polymtl.ca>
2021-09-26 22:17:13 -0400

    Added unit test

--- client/src/app/components/size-selector/size-selector/size-selector.component.spec.ts
+++ client/src/app/components/size-selector/size-selector/size-selector.component.spec.ts
@@ -1,7 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatIcon } from '@angular/material/icon';
-
 import { SizeSelectorComponent } from './size-selector.component';
+
+const MAX_SIZE = 35;
+const MIN_SIZE = 25;
+const DEFAULT_SIZE = 30;
 
 describe('SizeSelectorComponent', () => {
     let component: SizeSelectorComponent;
@@ -16,10 +19,35 @@ describe('SizeSelectorComponent', () => {
     beforeEach(() => {
         fixture = TestBed.createComponent(SizeSelectorComponent);
         component = fixture.componentInstance;
+
+        component.maxSize = MAX_SIZE;
+        component.minSize = MIN_SIZE;
+        component.size = DEFAULT_SIZE;
+
         fixture.detectChanges();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should increase until limit', () => {
+        for (let i = 0; i < MAX_SIZE - DEFAULT_SIZE; i++) {
+            component.increase();
+        }
+        expect(component.size).toEqual(MAX_SIZE);
+
+        component.increase();
+        expect(component.size).toEqual(MAX_SIZE);
+    });
+
+    it('should decrease until limit', () => {
+        for (let i = 0; i < DEFAULT_SIZE - MIN_SIZE; i++) {
+            component.decrease();
+        }
+        expect(component.size).toEqual(MIN_SIZE);
+
+        component.decrease();
+        expect(component.size).toEqual(MIN_SIZE);
     });
 });
