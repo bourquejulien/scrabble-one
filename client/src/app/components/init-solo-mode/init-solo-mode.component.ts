b5933a6f8a5c67a1fb498a2a543d19909df4cff2
Morgan De Gregorio Beaudoin <morgan.de-gregorio-beaudoin@polymtl.ca>
2021-09-27 22:43:48 -0400

    Now reacts in realtime for same name with bot and forces 0 seconds if 5 minutes is selected

--- client/src/app/components/init-solo-mode/init-solo-mode.component.ts
+++ client/src/app/components/init-solo-mode/init-solo-mode.component.ts
@@ -62,7 +62,11 @@ export class InitSoloModeComponent implements OnInit {
         }
     }
 
-    private botNameChange(firstPlayerName: string): void {
+    forceSecondsToZero(): void {
+        if (this.minutes === TURN_LENGTH_MINUTES[5]) this.seconds = 0;
+    }
+
+    botNameChange(firstPlayerName: string): void {
         while (firstPlayerName === this.gameConfig.secondPlayerName) {
             this.gameConfig.secondPlayerName = this.randomizeBotName(BOT_NAMES);
         }
