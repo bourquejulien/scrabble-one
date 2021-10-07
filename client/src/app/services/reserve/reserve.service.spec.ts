a93b12ac7caa6e47bee9f1b86c3a809d94ca9002
Ikram Kohil <ikram.kohil@polymtl.ca>
2021-10-07 17:10:02 +0000

    Update reserve.service.spec.ts

--- client/src/app/services/reserve/reserve.service.spec.ts
+++ client/src/app/services/reserve/reserve.service.spec.ts
@@ -2,7 +2,7 @@
 import { TestBed } from '@angular/core/testing';
 import { ReserveService } from '@app/services/reserve/reserve.service';
 
-fdescribe('ReserveService', () => {
+describe('ReserveService', () => {
     let letterToExchange: string;
     let service: ReserveService;
 
