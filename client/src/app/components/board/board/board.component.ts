421bb6b3b9919f4a117716f0ce4cb1f4c3398772
AlexDufort <alexandre.dufort@polymtl.ca>
2021-10-26 18:03:54 -0400

    backspace now working

--- client/src/app/components/board/board/board.component.ts
+++ client/src/app/components/board/board/board.component.ts
@@ -26,7 +26,7 @@ export class BoardComponent implements OnChanges, AfterViewInit {
     isFocus: boolean;
     squareSelected: boolean = false;
     isHorizontal: boolean = true;
-    position: Vec2;
+    gridPosition: Vec2;
     positionInit: Vec2;
     private gridContext: CanvasRenderingContext2D;
     private squareContext: CanvasRenderingContext2D;
@@ -36,47 +36,50 @@ export class BoardComponent implements OnChanges, AfterViewInit {
 
     @HostListener('body:mousedown', ['$event'])
     onMouseDown(event: MouseEvent): void {
-        console.log(this.rackService.rack);
         this.gridService.resetCanvas(this.tempContext);
         if (this.isFocus) {
             this.mouseHandlingService.mouseHitDetect(event);
-            if (this.position === undefined) {
-                this.position = this.mouseHandlingService.position;
-                this.positionInit = this.mouseHandlingService.position;
+            if (this.gridPosition === undefined) {
+                this.gridPosition = this.mouseHandlingService.position;
+                this.positionInit = { x: this.gridPosition.x, y: this.gridPosition.y };
             } else {
                 this.samePosition(this.mouseHandlingService.position);
             }
-            if (this.position.x > 0 && this.position.y > 0) {
-                this.gridService.drawSelectionSquare(this.tempContext, this.position);
-                this.gridService.drawDirectionArrow(this.tempContext, this.position, this.isHorizontal);
+            if (this.gridPosition.x > 0 && this.gridPosition.y > 0) {
+                this.gridService.drawSelectionSquare(this.tempContext, this.gridPosition);
+                this.gridService.drawDirectionArrow(this.tempContext, this.gridPosition, this.isHorizontal);
                 this.squareSelected = true;
             }
+        } else {
+            this.gridPosition.x = 0;
+            this.gridPosition.y = 0;
         }
     }
 
     @HostListener('body:keydown', ['$event'])
     onKeyDown(event: KeyboardEvent): void {
-        if (event.key === 'Backspace' && this.isPositionInit()) {
-            this.gridService.clearSquare(this.tempContext, this.position);
-            if (this.isHorizontal && this.position.x > MIN_SIZE) {
-                this.position.x -= 1;
-            } else if (!this.isHorizontal && this.position.y > MIN_SIZE) {
-                this.position.y -= 1;
+        console.log(this.isPositionInit());
+        if (event.key === 'Backspace' && !this.isPositionInit()) {
+            this.gridService.clearSquare(this.tempContext, this.gridPosition);
+            if (this.isHorizontal && this.gridPosition.x > MIN_SIZE) {
+                this.gridPosition.x -= 1;
+            } else if (!this.isHorizontal && this.gridPosition.y > MIN_SIZE) {
+                this.gridPosition.y -= 1;
             }
-            this.gridService.drawSelectionSquare(this.tempContext, this.position);
-            this.gridService.drawDirectionArrow(this.tempContext, this.position, this.isHorizontal);
+            this.gridService.drawSelectionSquare(this.tempContext, this.gridPosition);
+            this.gridService.drawDirectionArrow(this.tempContext, this.gridPosition, this.isHorizontal);
         } else {
             this.handleKeyPress(event.key);
             if (this.squareSelected === true && this.isLetter) {
-                this.gridService.cleanSquare(this.tempContext, this.position);
-                this.gridService.drawSymbol(event.key, this.position, this.tempContext);
-                if (this.isHorizontal && this.position.x < MAX_SIZE) {
-                    this.position.x += 1;
-                } else if (!this.isHorizontal && this.position.y < MAX_SIZE) {
-                    this.position.y += 1;
+                this.gridService.cleanSquare(this.tempContext, this.gridPosition);
+                this.gridService.drawSymbol(event.key, this.gridPosition, this.tempContext);
+                if (this.isHorizontal && this.gridPosition.x < MAX_SIZE) {
+                    this.gridPosition.x += 1;
+                } else if (!this.isHorizontal && this.gridPosition.y < MAX_SIZE) {
+                    this.gridPosition.y += 1;
                 }
-                this.gridService.drawSelectionSquare(this.tempContext, this.position);
-                this.gridService.drawDirectionArrow(this.tempContext, this.position, this.isHorizontal);
+                this.gridService.drawSelectionSquare(this.tempContext, this.gridPosition);
+                this.gridService.drawDirectionArrow(this.tempContext, this.gridPosition, this.isHorizontal);
             }
         }
     }
@@ -139,17 +142,17 @@ export class BoardComponent implements OnChanges, AfterViewInit {
     }
 
     private samePosition(position: Vec2) {
-        if (this.position.x === position.x && this.position.y === position.y) {
+        if (this.gridPosition.x === position.x && this.gridPosition.y === position.y) {
             this.isHorizontal = false;
         } else {
-            this.position = position;
-            this.positionInit = position;
+            this.gridPosition = position;
+            this.positionInit = { x: position.x, y: position.y };
             this.isHorizontal = true;
         }
     }
 
     private isPositionInit(): boolean {
-        if (this.position.x === this.positionInit.x && this.position.y === this.positionInit.y) {
+        if (this.gridPosition.x === this.positionInit.x && this.gridPosition.y === this.positionInit.y) {
             return true;
         } else return false;
     }
