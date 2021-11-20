54e14cb42b08a6d0981ae11d6b76674c50186629
Ikram Kohil <ikram.kohil@polymtl.ca>
2021-11-20 17:20:32 -0500

    Fix failing test in best score component

--- client/src/app/components/best-scores/best-scores.component.spec.ts
+++ client/src/app/components/best-scores/best-scores.component.spec.ts
@@ -1,33 +1,25 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatDialog } from '@angular/material/dialog';
+
 import { BestScoresComponent } from './best-scores.component';
 
-class MatDialogStub {
-    close() {
-        // Does Nothing
-    }
-}
-
 describe('BestScoresComponent', () => {
-    let component: BestScoresComponent;
-    let fixture: ComponentFixture<BestScoresComponent>;
+  let component: BestScoresComponent;
+  let fixture: ComponentFixture<BestScoresComponent>;
 
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            declarations: [BestScoresComponent],
-            imports: [HttpClientTestingModule],
-            providers: [{ provide: MatDialog, useClass: MatDialogStub }],
-        }).compileComponents();
-    });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BestScoresComponent ]
+    })
+    .compileComponents();
+  });
 
-    beforeEach(() => {
-        fixture = TestBed.createComponent(BestScoresComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
-    });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BestScoresComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
-    });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
--- client/src/app/components/best-scores/best-scores.component.ts
+++ client/src/app/components/best-scores/best-scores.component.ts
@@ -1,29 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { ScoreboardService } from '@app/services/scoreboard/scoreboard.service';
-import { Score } from '@common';
-
-const COLLECTION_NAME_CLASSIC = 'classic';
-const COLLECTION_NAME_LOG = 'log';
 
 @Component({
-    selector: 'app-best-scores',
-    templateUrl: './best-scores.component.html',
-    styleUrls: ['./best-scores.component.scss'],
+  selector: 'app-best-scores',
+  templateUrl: './best-scores.component.html',
+  styleUrls: ['./best-scores.component.scss']
 })
 export class BestScoresComponent implements OnInit {
-    classicBoardData: Score[];
-    logBoardData: Score[];
 
-    constructor(private scoreboardService: ScoreboardService) {
-        // this.initBoards();
-    }
+  constructor() { }
 
-    ngOnInit() {
-        this.initBoards();
-    }
+  ngOnInit(): void {
+  }
 
-    async initBoards(): Promise<void> {
-        this.classicBoardData = await this.scoreboardService.displayScores(COLLECTION_NAME_CLASSIC);
-        this.logBoardData = await this.scoreboardService.displayScores(COLLECTION_NAME_LOG);
-    }
 }
