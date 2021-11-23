d451a6328f199127d1012d11ca3982f66eb5e70e
Morgan De Gregorio Beaudoin <morgan.de-gregorio-beaudoin@polymtl.ca>
2021-11-23 14:00:53 -0500

    flex on container for objectives

--- client/src/app/components/objectives/objectives.component.spec.ts
+++ client/src/app/components/objectives/objectives.component.spec.ts
@@ -1,7 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ObjectivesComponent } from './objectives.component';
 
-
 describe('ObjectivesComponent', () => {
     let component: ObjectivesComponent;
     let fixture: ComponentFixture<ObjectivesComponent>;
