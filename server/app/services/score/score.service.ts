6417678363cd719fbf4eee84136dd5af0fd262e1
Julien Bourque <julien.bourque@polymtl.ca>
2021-11-28 23:38:40 -0500

    Removed collection names from StatsService

--- server/app/services/score/score.service.ts
+++ server/app/services/score/score.service.ts
@@ -1,41 +1,38 @@
 import { DatabaseService } from '@app/services/database/database.service';
-import { Score } from '@common';
+import { GameMode, Score } from '@common';
 import { Collection } from 'mongodb';
 import { Service } from 'typedi';
-import { Constants } from '@app/constants';
 
 const MAX_DOCUMENTS = 25;
 
+const DATABASE_COLLECTION_CLASSIC = 'classicScoreboard';
+const DATABASE_COLLECTION_LOG = 'logScoreboard';
+
 @Service()
 export class ScoreService {
-    private classicScoreboard: Collection<Score>;
-    private logScoreboard: Collection<Score>;
+    private readonly classicScoreboard: Collection<Score>;
+    private readonly logScoreboard: Collection<Score>;
 
     constructor(private databaseService: DatabaseService) {
-        this.classicScoreboard = this.databaseService.scrabbleDb.collection(Constants.DATABASE_COLLECTION_CLASSIC);
-        this.logScoreboard = this.databaseService.scrabbleDb.collection(Constants.DATABASE_COLLECTION_LOG);
+        this.classicScoreboard = this.databaseService.scrabbleDb.collection(DATABASE_COLLECTION_CLASSIC);
+        this.logScoreboard = this.databaseService.scrabbleDb.collection(DATABASE_COLLECTION_LOG);
     }
 
-    async updateScoreboard(score: Score[], collectionName: string): Promise<void> {
-        this.getCollection(collectionName).insertMany(score);
+    async updateScoreboard(score: Score[], gameMode: GameMode): Promise<void> {
+        await this.getCollection(gameMode).insertMany(score);
     }
 
-    async isPlayerInScoreboard(playerName: string, collectionName: string): Promise<boolean> {
-        const foundPlayer = await this.getCollection(collectionName).findOne({ name: playerName });
+    async isPlayerInScoreboard(playerName: string, gameMode: GameMode): Promise<boolean> {
+        const foundPlayer = await this.getCollection(gameMode).findOne({ name: playerName });
         return foundPlayer !== null;
     }
 
-    async deleteElement(playerName: string, collectionName: string): Promise<void> {
-        await this.getCollection(collectionName).deleteOne({ name: playerName });
+    async deleteElement(playerName: string, gameMode: GameMode): Promise<void> {
+        await this.getCollection(gameMode).deleteOne({ name: playerName });
     }
 
-    async isScoreUnique(scoreVal: number, collectionName: string): Promise<boolean> {
-        const scoreFound = await this.getCollection(collectionName).findOne({ scoreValue: scoreVal });
-        return scoreFound === null;
-    }
-
-    async getPlayerScore(playerName: string, collectionName: string): Promise<number> {
-        return this.getCollection(collectionName)
+    async getPlayerScore(playerName: string, gameMode: GameMode): Promise<number> {
+        return this.getCollection(gameMode)
             .findOne({ name: playerName })
             .then((score) => {
                 return score == null ? -1 : score.scoreValue;
@@ -50,7 +47,7 @@ export class ScoreService {
         return this.logScoreboard.find().limit(MAX_DOCUMENTS).sort({ scoreValue: -1 }).toArray();
     }
 
-    private getCollection(collectionName: string): Collection<Score> {
-        return collectionName === Constants.DATABASE_COLLECTION_CLASSIC ? this.classicScoreboard : this.logScoreboard;
+    private getCollection(gameMode: GameMode): Collection<Score> {
+        return gameMode === GameMode.Classic ? this.classicScoreboard : this.logScoreboard;
     }
 }
