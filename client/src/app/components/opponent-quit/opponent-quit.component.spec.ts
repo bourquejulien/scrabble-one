ff34b21a0591af94df0122827bb957544e11811b
Ikram Kohil <ikram.kohil@polymtl.ca>
2021-12-01 22:42:06 -0500

    Full coverage in opponent-quit component

--- client/src/app/components/opponent-quit/opponent-quit.component.spec.ts
+++ client/src/app/components/opponent-quit/opponent-quit.component.spec.ts
@@ -1,3 +1,7 @@
+/* eslint-disable dot-notation -- we need access to private properties for the test */
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { Injectable } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
@@ -8,7 +12,7 @@ import { OpponentQuitComponent } from './opponent-quit.component';
     providedIn: 'root',
 })
 class MatDialogStub {
-    close() {
+    closeAll() {
         // Does Nothing
     }
 }
@@ -22,11 +26,11 @@ describe('OpponentQuitComponent', () => {
         gameServiceSpy = jasmine.createSpyObj('GameService', ['reset']);
         await TestBed.configureTestingModule({
             declarations: [OpponentQuitComponent],
-            imports: [ HttpClientTestingModule, MatDialogModule ],
-            providers: [ 
+            imports: [HttpClientTestingModule, MatDialogModule],
+            providers: [
                 { provide: MatDialogRef, useClass: MatDialogStub },
-                { provide: GameService, useValue: gameServiceSpy},
-                { provide: MAT_DIALOG_DATA, useValue: {}}, 
+                { provide: GameService, useValue: gameServiceSpy },
+                { provide: MAT_DIALOG_DATA, useValue: {} },
             ],
         }).compileComponents();
     });
@@ -41,5 +45,20 @@ describe('OpponentQuitComponent', () => {
         expect(component).toBeTruthy();
     });
 
-});
+    it('should reset game if quit', () => {
+        component.quit();
+        expect(gameServiceSpy['reset']).toHaveBeenCalled();
+    });
 
+    it('should close all dialogs if quit', () => {
+        const spy = spyOn<any>(component['dialogRef'], 'closeAll');
+        component.quit();
+        expect(spy).toHaveBeenCalled();
+    });
+
+    it('should close all dialogs if continue', () => {
+        const spy = spyOn<any>(component['dialogRef'], 'closeAll');
+        component.continue();
+        expect(spy).toHaveBeenCalled();
+    });
+});
